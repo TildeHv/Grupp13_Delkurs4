218d484ae0abb8b2cf47deee28bafacde5b750b7
mochyrie <dianoora@hotmail.com>
2026-01-05 13:47:02 +0100

    Initial commit

---
+++ src/ngo_2024/ProjektKlass.java
@@ -0,0 +1,117 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ngo_2024;
+
+import oru.inf.InfDB;
+import oru.inf.InfException;
+import java.util.HashMap;
+/**
+ *
+ * @author user
+ */
+public class ProjektKlass {
+    
+    private InfDB idb; 
+    private String InloggadAnvandare;
+    private String projektnamn;
+    private String beskrivning;
+    private String startdatum;
+    private String slutdatum;
+    private String kostnad; 
+    private String status;
+    private String projektchef; 
+    private String land; 
+    private String prioritet;
+    private String pid; 
+    
+public ProjektKlass (InfDB idb, String InloggadAnvandare, String projektnamn) {
+    
+    this.InloggadAnvandare = InloggadAnvandare;
+    this.idb = idb;
+    this.projektnamn = projektnamn;
+    hamtaProjekt();
+}
+
+
+private void hamtaProjekt() {
+        
+    try {
+        
+        String sqlFraga = 
+        "SELECT pid, projektnamn, beskrivning, startdatum, slutdatum, kostnad, status, prioritet, projektchef, land " + // OBS: mellanslag här
+        "FROM projekt " +
+        "WHERE projektnamn = '" + projektnamn + "'";
+        
+        HashMap<String, String> rad = idb.fetchRow(sqlFraga);
+          
+        
+      if (rad == null) {
+          System.out.println("Error");        
+   } else {
+          System.out.println("Lyckades!");
+      
+          pid = rad.get("pid");
+          projektnamn = rad.get("projektnamn");
+          beskrivning = rad.get ("beskrivning");
+          startdatum = rad.get ("startdatum");
+          slutdatum = rad.get ("slutdatum");
+          kostnad = rad.get ("kostnad");
+          status = rad.get ("status");
+          prioritet = rad.get("prioritet");
+          projektchef = rad.get("projektchef");
+          land = rad.get("land");
+          
+
+      }      
+  
+    } catch (InfException e) {
+        e.printStackTrace();
+    }
+}
+
+
+    public String getPid() {
+    return pid;
+}
+    
+    public String getProjektnamn() {
+    return projektnamn;
+}
+     
+    public String getBeskrivning() {
+    return beskrivning;
+}
+      
+    public String getKostnad() {
+    return kostnad;
+}
+       
+   public String getStartdatum() {
+    return startdatum;
+}
+       
+   public String getSlutdatum() {
+    return slutdatum;
+}
+       
+   public String getProjektchef() {
+    return projektchef;
+    
+}
+   public String getLand() {
+    return land;
+    
+}
+   public String getPrioritet() {
+    return prioritet;
+}
+   
+   public String getStatus() {
+    return status;
+   }
+}
+       
+       
+       
