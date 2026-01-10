f8cf5abaf03e5042be2ea410efaf3b1ae81863aa
mochyrie <dianoora@hotmail.com>
2026-01-10 12:42:05 +0100

    Initial commit

--- src/ngo_2024/Validering.java
+++ src/ngo_2024/Validering.java
@@ -4,35 +4,26 @@
  */
 package ngo_2024;
 
-import oru.inf.InfDB;
-import oru.inf.InfException;
-/**
- */
 public class Validering {
-    public static boolean ValideraEpost(String ePost) { 
-       return ePost.matches ("^[a-zA-Z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$");  // ^början $slut.  a-z tillåter bokstäver förutomåäö. 
+
+    public static boolean ValideraEpost(String ePost) {
+        return ePost.matches("^[a-zA-Z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$");  // ^början $slut.  a-z tillåter bokstäver förutomåäö. 
     }
-    
-    public static boolean ValideraTelefon(String telefon){
-        return telefon.matches ("^\\+?[0-9]{7,15}$"); 
+
+    public static boolean ValideraTelefon(String telefon) {
+        return telefon.matches("^\\+?[0-9]{7,15}$");
     }
-    
-    public static boolean ValideraAdress(String adress) { 
-        return adress.matches ("^[A-Za-zÅÄÖåäö0-9 .\\-]{5,50}$"); 
+
+    public static boolean ValideraAdress(String adress) {
+        return adress.matches("^(?=.*[0-9])[A-Za-zÅÄÖåäö0-9 .\\-,/]{5,50}$");
     }
-    
-   public static boolean ValideraNamn(String namn){
-       return namn.matches ("^[A-Za-zÅÄÖåäö'\\ -]{2,40}$");
-   }
-   
-   public static boolean ValideraDatum(String datum) {
-       return datum.matches ("\\d{4}-\\d{2}-\\d{2}");
-   }
-    
+
+    public static boolean ValideraNamn(String namn) {
+        return namn.matches("^[A-Za-zÅÄÖåäö'\\ -]{2,40}$");
+    }
+
+    public static boolean ValideraDatum(String datum) {
+        return datum.matches("\\d{4}-\\d{2}-\\d{2}");
+    }
+
 }
-
-
-
-    
-
-
